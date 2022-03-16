c237869469de1812747c4de32922736602ff1e1d
BollikondaSivaKrishna123 <64198989+BollikondaSivaKrishna123@users.noreply.github.com>
2022-03-16 10:25:13 +0530

    Add files via upload
    ravi work

---
+++ src/main/java/com/cts/SpringBootCloudAppApplication.java
@@ -0,0 +1,18 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCloudAppApplication.class, args);
+		System.out.println("Hello , Welcome to AWS Service");
+		
+		
+		
+		
+	}
+
+}
